eec517d255289ffbbc98d8be4da441217c6e9302
Carsten Daurehøj <arelstone@gmail.com>
2022-04-10 15:55:48 +0200

    🐛 Fix #10 (#11)

--- src/steerlock/steerlock.command.ts
+++ src/steerlock/steerlock.command.ts
@@ -3,10 +3,11 @@ import {
   Command,
   DiscordTransformedCommand,
   Payload,
+  TransformedCommandExecutionContext,
   UseFilters,
   UsePipes,
 } from '@discord-nestjs/core';
-import { Injectable, ValidationPipe } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CommandValidationFilter } from 'src/filters/command-validation.filter';
 import { SteerlockDto } from './steerlock.dto';
 import data from './steerlocks.json';
@@ -16,18 +17,26 @@ import data from './steerlocks.json';
   name: 'steerlock',
   description: 'Need to know the steerlock of a car?',
 })
-@UsePipes(TransformPipe, ValidationPipe)
+@UsePipes(TransformPipe)
 @UseFilters(CommandValidationFilter)
 export class SteerlockCommand
   implements DiscordTransformedCommand<SteerlockDto>
 {
-  async handler(@Payload() { car }: SteerlockDto): Promise<string> {
+  private readonly logger = new Logger(SteerlockCommand.name);
+
+  async handler(
+    @Payload() { car }: SteerlockDto,
+    { interaction: { user } }: TransformedCommandExecutionContext<any>,
+  ): Promise<string> {
+    this.logger.debug(`Finding steerlock for (car: ${car}, user: ${user.tag})`);
+
     const response = data
-      .filter((d) => d.car.toLowerCase().includes(car.toLowerCase()))
+      .filter((d) => d.car.toUpperCase().includes(car))
       .map((r) => `Steerlock for: **${r.car}** is **${r.value}**`)
       .join('\n');
 
     if (!response) {
+      this.logger.debug(`Data was not found`);
       return 'Car not found';
     }
 
