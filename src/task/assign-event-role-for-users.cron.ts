df8f01b3d5e029d2dd72c10ac8427746114bcfd9
Carsten Daurehøj <arelstone@gmail.com>
2022-06-16 14:48:49 +0200

    run cron job every hour

--- src/task/assign-event-role-for-users.cron.ts
+++ src/task/assign-event-role-for-users.cron.ts
@@ -18,7 +18,7 @@ export class AssignEventRoleForUsers {
     private readonly log: LoggingChannelService,
   ) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_HOUR)
   async handleCron() {
     this.logger.debug('Running task: `Assign role to user for simgrid events`');
 
