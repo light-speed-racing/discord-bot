68a05655a9f5075fee944aa57ee611db3afeeb50
Carsten Daurehøj <arelstone@gmail.com>
2022-06-17 16:39:08 +0200

    🐛 Dont prank Spencer that much

--- src/guards/user-said.guard.ts
+++ src/guards/user-said.guard.ts
@@ -6,15 +6,11 @@ export class UserSaidGuard implements DiscordGuard {
   private readonly words: string[];
 
   constructor(...words: string[]) {
-    this.words = words.map(this.addWhiteSpace);
+    this.words = words;
   }
 
   @UseGuards(MessageIsFromUserGuard)
   canActive(event: 'messageCreate', [message]: [Message]): boolean {
     return this.words.some((word) => message.content.includes(word));
-  }
-
-  private addWhiteSpace(word: string) {
-    return ` ${word} `;
   }
 }
