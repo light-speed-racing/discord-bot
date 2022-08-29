75774436fc1fea218b7b1ba867da657f00fbf575
Carsten Daurehøj <arelstone@gmail.com>
2022-08-29 12:43:41 +0200

    Update championships

--- src/championships.ts
+++ src/championships.ts
@@ -1,19 +1,17 @@
 export enum Championships {
-  'Summer Challange' = '1366',
-  'Mixed Sundays S2' = '1435',
+  'Mixed Sundays S3' = '1918',
   'Endurance S1' = '1266',
   'Midweek League S2' = '1449',
-  'Comming to America' = '1734',
   '82 cars at Spa' = '1746',
   '12 hours at Spa' = '1660',
+  '105 cars at Suzuka' = '1917',
 }
 
 export const championshipRoles: Record<Championships, string> = {
-  [Championships['Summer Challange']]: 'Summer Challenge',
-  [Championships['Mixed Sundays S2']]: 'Mixed Sundays S2',
+  [Championships['Mixed Sundays S3']]: 'Mixed Sundays S3',
   [Championships['Endurance S1']]: 'Endurance S1',
   [Championships['Midweek League S2']]: 'Midweek League S2',
-  [Championships['Comming to America']]: 'One off - Comming to America',
-  [Championships['82 cars at Spa']]: 'One off - Fill the pool',
   [Championships['12 hours at Spa']]: 'Endurance - 12h at Spa',
+  [Championships['82 cars at Spa']]: 'One off - Fill the pool',
+  [Championships['105 cars at Suzuka']]: 'One off - Running Sushi at Suzuka',
 };
