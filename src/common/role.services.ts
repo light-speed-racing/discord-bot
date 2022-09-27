c486f9d6f06ef0112dc36e70a024d9c860506cd3
Carsten Daurehøj <arelstone@gmail.com>
2022-09-27 08:25:56 +0200

    🥅

--- src/common/role.services.ts
+++ src/common/role.services.ts
@@ -1,35 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { GuildMember, Role } from 'discord.js';
 import { GuildService } from './guild.services';
 
 @Injectable()
 export class RoleService {
+  private readonly logger = new Logger(RoleService.name);
+
   constructor(private readonly guild: GuildService) {}
 
   async has(member: GuildMember, rolename: string): Promise<boolean> {
     const role = await this.findByName(rolename);
-
-    return (await member.fetch(true)).roles.cache.has(role?.id);
+    try {
+      return (await member.fetch(true)).roles.cache.has(role?.id);
+    } catch (error) {
+      this.logger.error('has: Could not fetch role of members', {
+        member,
+        rolename,
+        error,
+      });
+    }
   }
 
   async findByName(name: string): Promise<Role | null> {
-    const role = (await this.guild.guild.roles.fetch()).find(
-      (role) => role.name.toLowerCase() === name.toLowerCase(),
-    );
+    try {
+      const role = (await this.guild.guild.roles.fetch()).find(
+        (role) => role.name.toLowerCase() === name.toLowerCase(),
+      );
 
-    return role;
+      return role;
+    } catch (error) {
+      this.logger.error('findByName: Could not find', { name, error });
+    }
   }
 
   async create(name: string): Promise<Role> {
-    const role = await this.guild.guild.roles.create({
-      name,
-      color: '#088bbf',
-    });
+    try {
+      const role = await this.guild.guild.roles.create({
+        name,
+        color: '#088bbf',
+      });
 
-    return role;
+      return role;
+    } catch (error) {
+      this.logger.error('create: Could create role', { name, error });
+    }
   }
 
   async exists(name: string): Promise<boolean> {
-    return !!(await this.findByName(name));
+    try {
+      return !!(await this.findByName(name));
+    } catch (error) {}
   }
 }
