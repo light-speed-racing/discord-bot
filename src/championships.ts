cdaa90c4fa493d321b78c383fb7f9ddb6d320767
Carsten Daurehøj <arelstone@gmail.com>
2022-10-23 23:56:09 +0200

    Add Winter Challenge role

--- src/championships.ts
+++ src/championships.ts
@@ -16,10 +16,10 @@ export const championships: ReadonlyArray<Championship> = [
   { id: 2023, role: 'Züper Zundayz' },
   { id: 1266, role: 'Endurance S1', driverSwap: true },
   { id: 1449, role: 'Midweek League S2' },
-  { id: 1746, role: 'One off - Fill the pool' },
   { id: 1917, role: 'One off - Running Sushi at Suzuka' },
   { id: 1746, role: 'Endurance - 12h at Spa', driverSwap: true },
   { id: 2014, role: 'Kyalami 1000k', driverSwap: true },
   { id: 2084, role: 'Versus Thursdays' },
   { id: 2272, role: 'Eco Lovers Spa Day' },
+  { id: 2332, role: 'Winter Challenge' },
 ];
