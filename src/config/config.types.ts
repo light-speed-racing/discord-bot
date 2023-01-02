fd8d340f55d1ccaf146dbbc7eec7aa1a1fc87c71
Carsten Daurehøj <arelstone@gmail.com>
2023-01-02 17:26:46 +0100

    commit

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,13 +1,11 @@
 import { DiscordConfig } from './discord.config';
 import { BaseConfig } from './base.config';
 import { ApiKeysConfig } from './apiKeys.config';
-import { SimgridConfig } from './simgrid.config';
 
 export type Config = {
   discord: DiscordConfig;
   base: BaseConfig;
   apiKeys: ApiKeysConfig;
-  simgrid: SimgridConfig;
 };
 
-export { DiscordConfig, BaseConfig, ApiKeysConfig, SimgridConfig };
+export { DiscordConfig, BaseConfig, ApiKeysConfig };
