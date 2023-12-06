be7fde6bb7719c01591b7ac8f6879abc7a98a7ae
Carsten Daurehøj <arelstone@gmail.com>
2023-12-06 17:35:07 +0100

    Greta is very mad

--- src/user-said/anyone-says-anything.event.ts
+++ src/user-said/anyone-says-anything.event.ts
@@ -8,13 +8,24 @@ import { Likelihood } from 'src/guard/likelyhood.guard';
 @Injectable()
 export class AnyoneSaysAnythingEvent {
   @On('messageCreate')
-  @UseGuards(MessageIsSendByAUser, new Likelihood(0.0001))
+  @UseGuards(MessageIsSendByAUser, new Likelihood(1))
   async onMessage(message: Message): Promise<void> {
     const messages = [
-      'I am go mad at you! How dare you! Go hug a tree and give me proof or I will ban you! #furyofgreta',
-      'I will ban you!!',
+      `I am go mad at you! How dare you! Go hug a tree and give me proof or I will ban you! #furyofgreta`,
+      `I will ban you ${message.author}!!`,
       `How dare you say my name ${message.author}?!`,
-      `I own 100 private jets`,
+      `I own 100 private jets. What do you have ${message.author}? A gas driven car?!?`,
+      `You're a nuclear reactor to me ${message.author}`,
+      `${message.author} makes me want to glue myself onto the next road?`,
+      `${message.author}'s carbon footprint is bigger than their brain capacity!`,
+      `The only rise I see here is the rise in my frustration level due to your climate indifference.`,
+      `If ignorance was a greenhouse gas, you would be the main source!`,
+      `You're glued to fossil fuels, just like you're glued to ignorance, ${message.author}!`,
+      `${message.author}, I'd make your ass grass but garbage is bad for the environment`,
+      `You're treating the planet like it's your own personal garbage dump. Time for an eco-reality check!`,
+      `If excuses were renewable energy, you'd have solved the climate crisis by now, ${message.author}!`,
+      `${message.author}'s carbon footprint is so extravagant, it's like you're auditioning for a starring role in 'Planet Polluter: The Sequel.'`,
+      `I've seen more sustainable practices in a toddler's finger painting than in your daily routine!`,
     ];
     await message.reply(sample(messages));
   }
