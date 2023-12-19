172d562e8b1f4cd7c0842193e97bca305bd8301e
Carsten Daurehøj <arelstone@gmail.com>
2023-12-19 16:14:52 +0100

    asd

--- src/open-game-panel/commands/start-game-server.subcommand.ts
+++ src/open-game-panel/commands/start-game-server.subcommand.ts
@@ -1,29 +1,50 @@
-import { SubCommand, IA, On } from '@discord-nestjs/core';
+import { SubCommand, Handler, IA, EventParams, On } from '@discord-nestjs/core';
 import { GameManager } from '../game-manager.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GameServer } from 'src/database/game-server.entity';
-import { StringSelectMenuInteraction } from 'discord.js';
-import { ServerSelectMenu } from './select-menu-with-servers';
+import {
+  ActionRowBuilder,
+  ClientEvents,
+  Message,
+  StringSelectMenuBuilder,
+  StringSelectMenuInteraction,
+  StringSelectMenuOptionBuilder,
+} from 'discord.js';
+import { IsHost } from 'src/guard/is-host.guard';
+import { UseGuards } from '@nestjs/common';
 
 @SubCommand({
   name: 'start',
   description: 'Start a game server',
 })
-export class StartGameServerSubcommand extends ServerSelectMenu {
+export class StartGameServerSubcommand {
+  private allServers: Array<GameServer>;
   constructor(
     @InjectRepository(GameServer)
-    readonly repository: Repository<GameServer>,
+    private readonly repository: Repository<GameServer>,
     private readonly gameManager: GameManager,
-  ) {
-    super(repository);
-  }
+  ) {}
 
-  // @UseGuards(IsHost)
-  // @Handler()
-  // async handle(@EventParams() [interaction]: ClientEvents['interactionCreate']): Promise<Message> {
-  //   return this._handle(interaction);
-  // }
+  @UseGuards(IsHost)
+  @Handler()
+  async handle(@EventParams() [interaction]: ClientEvents['interactionCreate']): Promise<Message> {
+    this.allServers = await this.repository.find();
+
+    const server = new StringSelectMenuBuilder()
+      .setCustomId('gameServer')
+      .setPlaceholder('Select a server')
+      .addOptions(
+        this.allServers.map(({ home_name }, index) => {
+          return new StringSelectMenuOptionBuilder().setLabel(home_name).setValue(`${index}`);
+        }),
+      );
+
+    // @ts-expect-error sdfdsf
+    return await interaction.reply({
+      components: [new ActionRowBuilder().addComponents(server)],
+    });
+  }
 
   @On('interactionCreate')
   async onSubmit(@IA() { customId, values, message }: StringSelectMenuInteraction) {
