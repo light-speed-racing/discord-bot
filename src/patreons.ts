f46118af53865c7549aa88baabb88bae584c8043
Carsten Daurehøj <arelstone@gmail.com>
2024-09-12 14:50:03 +0200

    Update patreons

--- src/patreons.ts
+++ src/patreons.ts
@@ -16,16 +16,11 @@ export const Patreons = new Map<SteamId, Patreon>()
   .set(76561199249817935, { raceNumber: 188, discordId: 838598784597360700, steamId: 'S76561199249817935' }) // Paulo Taco
   .set(76561199274976963, { raceNumber: 12, discordId: 998579424813391942, steamId: 'S76561199274976963' }) // Andrea Furlan
   .set(76561199147180015, { raceNumber: 14, discordId: 821928912635428904, steamId: 'S76561199147180015' }) // Spencer
-  .set(76561198118872521, { raceNumber: 15, discordId: 406060014919221249, steamId: 'S76561198118872521' }) // Rick Pink
   .set(76561198079364851, { raceNumber: 32, discordId: 367427290428080148, steamId: 'S76561198079364851' }) // Ryan
   .set(76561199076112515, { raceNumber: 37, discordId: 725368578479489037, steamId: 'S76561199076112515' }) // Ferry
-  .set(76561197985618443, { raceNumber: 75, discordId: 501092515542466570, steamId: 'S76561197985618443' }) // Jonas
   .set(76561198112678444, { raceNumber: 16, discordId: 721066327355424788, steamId: 'S76561198112678444' }) // Pascal
   .set(76561199381496111, { raceNumber: 58, discordId: 990492981796683876, steamId: 'S76561199381496111' }) // ayrton
-  .set(76561199654081632, { raceNumber: 199, discordId: 567757070426701857, steamId: 'S76561199654081632' }) // Mark Boyle
-  .set(76561199476122242, { raceNumber: 333, discordId: 1078851572932558988, steamId: 'S76561199476122242' }) // Bryan Miller
+  .set(732305540268425269, { raceNumber: 46, discordId: 732305540268425269, steamId: 'S76561198166194464' }) // Mike Ranner
   .set(null, { raceNumber: 5, discordId: 285385820423323658, steamId: null }) // Jan Budde
   .set(null, { raceNumber: 7, discordId: 415517703256801280, steamId: null }) // Oliver
-  .set(null, { raceNumber: 57, discordId: 391218402376810496, steamId: null }) // Daan
-  .set(null, { raceNumber: 74, discordId: 680324364561940480, steamId: null }) // Scott
   .set(null, { raceNumber: 89, discordId: 838598784597360700, steamId: null }); // Shoebop
